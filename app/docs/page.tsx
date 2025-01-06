390326573594a14248a020049982814203912c7f
gguevara112 <guguevara@alumnos.uai.cl>
2025-01-06 00:39:13 -0300

    3rd

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,12 +1,11 @@
 import { title } from "@/components/primitives";
 import React from "react";
-import Pric  from "@/components/Pric"
 
 export default function DocsPage() {
   return (
     <div>
       <h1 className={title()}>Docs</h1>
-      <Pric />  
+
 
     </div>
   );
